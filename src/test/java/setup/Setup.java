5628e378b02fd2306598e3b52977360620da88cb
tanvirmitul <tanvirhmitul96@gmail.com>
2023-05-27 13:29:10 +0600

    update again

--- src/test/java/setup/Setup.java
+++ src/test/java/setup/Setup.java
@@ -1,6 +1,5 @@
 package setup;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -17,12 +16,9 @@ public class Setup {
     public WebDriver driver;
     @BeforeTest
     public void setup(){
-//        WebDriverManager.chromedriver().setup();
-//        ChromeOptions co= new ChromeOptions();
-//        co.addArguments("--remote-allow-origins=*");
-//        driver=new ChromeDriver(co);
-        WebDriverManager.firefoxdriver().setup();
-        driver=new FirefoxDriver();
+        ChromeOptions co= new ChromeOptions();
+        co.addArguments("--remote-allow-origins=*");
+        driver=new ChromeDriver(co);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     }
--- src/test/java/utils/Utils.java
+++ src/test/java/utils/Utils.java
@@ -4,18 +4,21 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-
+import org.testng.reporters.Files;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
 public class Utils {
     public void takeScreenShot(WebDriver driver) throws IOException {
+        driver.get("https://www.google.com/");
         File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        String time = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss-aa").format(new Date());
+        String time = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss-aa").format(new java.util.Date());
         String fileWithPath = "./src/test/resources/screenshots/" + time + ".png";
         File DestFile = new File(fileWithPath);
         FileUtils.copyFile(screenshotFile, DestFile);
+        driver.quit();
     }
 }
